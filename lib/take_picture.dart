a695a731a66a45f133674d2b96bb90a343204610
김동빈 <kdobi87@naver.com>
2025-12-23 14:21:04 +0900

    feat: initial commit

---
+++ lib/take_picture.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TakePicture extends StatefulWidget {
+  const TakePicture({super.key});
+
+  @override
+  State<TakePicture> createState() => _TakePictureState();
+}
+
+class _TakePictureState extends State<TakePicture> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
